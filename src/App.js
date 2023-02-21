3b42cc104dd09dd3b4afc7dcd0d3fdcbc0a9c14b
AbhishekSingh0029 <abhishek.singh.0029@gmail.com>
2023-02-21 11:14:25 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,1 @@
 
-import './App.css';
-import { useState } from 'react';
-import Signin from './Signin';
-function App() {
-  const [name,setName]=useState("aa");
-  return (
-    <div className="App">
-      
-        <Signin />
-        
-      </div>
-    
-  );
-}
-
-export default App;
